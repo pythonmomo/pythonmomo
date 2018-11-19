70b34dd51e64c7e53d36ffb6c013c9e50b0b5e3d
Allan Guwatudde <allanguwatudde@Allans-MacBook-Pro-2.local>
2018-11-19 16:15:15 +0300

    Initial setup of the pythonmomo package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pythonmomo",
+    version="0.0.1",
+    author="Abulo John Joshua, Allan Guwatudde",
+    author_email="john.abulo@andela.com, allan.guwatudde@andela.com",
+    description="A simple to use python wrapper to integrate the MTN Uganda API into your projects.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pythonmomo/pythonmomo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
